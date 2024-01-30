a659a424785b3c88dfdc9309a2af2d02e169fba7
lfneves <lfneves.dg@gmail.com>
2024-01-29 22:57:42 -0300

    build: adjusting build project

--- src/main/kotlin/com/mvp/order/OrderApplication.kt
+++ src/main/kotlin/com/mvp/order/OrderApplication.kt
@@ -2,6 +2,7 @@ package com.mvp.order
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Profile
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
